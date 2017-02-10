cf4ac022ba7b1cd5074160cab6a02070f32973ee
anpoulos <anpoulos@uwm.edu>
2017-02-09 19:22:20 -0600

    ActiveUser and Chatroom fully commented.

--- ChatLoco/ChatLoco/Classes/Chatroom/ActiveUser.cs
+++ ChatLoco/ChatLoco/Classes/Chatroom/ActiveUser.cs
@@ -10,7 +10,12 @@ namespace ChatLoco.Classes.Chatroom
     {
         public string Username { get; set; }
         public bool IsActive { get; set; }
+
+        //parent list that the user belongs to
+        //this list belongs to the chatroom that the user is in
+        //we need it so we can remove ourself from the list if necessary
         private Dictionary<string, ActiveUser> BelongsToUsersList;
+
         Timer IdleTimer;
 
         public ActiveUser(string username, bool isActive, Dictionary<string, ActiveUser> usersList)
@@ -19,13 +24,22 @@ namespace ChatLoco.Classes.Chatroom
             IsActive = isActive;
             BelongsToUsersList = usersList;
 
+            //create a timer that calls the IdleCheck method every 11 seconds
             IdleTimer = new Timer();
             IdleTimer.Elapsed += new ElapsedEventHandler(IdleCheck);
-            IdleTimer.Interval = 5000;
+            IdleTimer.Interval = 11000;
             IdleTimer.Enabled = true;
 
         }
 
+        //Remember that every 5 seconds, the user marks its IsActive flag as true. 
+        //This call happens from the user's JS ajax request when it asks for chatroom information (users list, chatrooms list)
+        //This method works by:
+        //If a user is marked as active, it marks them as inactive
+        //since the user is telling the chatroom they are active every five seconds, then by the next time this method is called (11seconds later), they should be active again
+        //if they have not informed the server that they are active, then the user object deletes itself.
+        //it does this by removing itself from the chatroom's user list, and marking all its references to null. 
+        //This allows garbage collection to delete the object.
         private void IdleCheck(object source, ElapsedEventArgs e)
         {
             if (!IsActive)
--- ChatLoco/ChatLoco/Classes/Chatroom/Chatroom.cs
+++ ChatLoco/ChatLoco/Classes/Chatroom/Chatroom.cs
@@ -8,11 +8,12 @@ namespace ChatLoco.Classes.Chatroom
     public class Chatroom
     {
         public int Id { get; set; }
-        public List<string> AllMessages = new List<string>();
-        public Dictionary<string, ActiveUser> AllUsers = new Dictionary<string, ActiveUser>();
+        public List<string> AllMessages = new List<string>(); //list of all formatted messages
+        public Dictionary<string, ActiveUser> AllUsers = new Dictionary<string, ActiveUser>(); //list of currently active users
         public string Name { get; set; }
-        public Dictionary<string, Chatroom> AllSubChatrooms = new Dictionary<string, Chatroom>();
-
+        public Dictionary<string, Chatroom> AllSubChatrooms = new Dictionary<string, Chatroom>(); //list of private chatrooms attached to this chatroom
+        
+        //force constructor to define name since that is the key that is used in the key value dictionary lists with chatrooms
         public Chatroom(string name)
         {
             Name = name;
@@ -20,6 +21,7 @@ namespace ChatLoco.Classes.Chatroom
 
         public bool HasUser(string Username)
         {
+            //try catch needed since referencing directly with key from a dictionary
             try
             {
                 var User = AllUsers[Username];
@@ -34,13 +36,14 @@ namespace ChatLoco.Classes.Chatroom
         public void AddUser(string Username)
         {
             ActiveUser user = new ActiveUser(Username, true, AllUsers);
-            try
+            try //try catch needed here because the user might possibly already exist in the users list, throwing a exception
             {
                 AllUsers.Add(Username, user);
             }
             catch (Exception e) { }
         }
 
+        //This method is used to denote that the user is still active
         public void UpdateUsers(string Username)
         {
             AllUsers[Username].IsActive = true;
