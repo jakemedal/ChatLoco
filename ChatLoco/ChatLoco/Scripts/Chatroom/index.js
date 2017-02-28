b0ea5c54541ca26d38c46e3ada691ed65a57a560
Jake Medal <jake.medal@gmail.com>
2017-02-28 16:12:32 -0600

    Added map with markers for nearby places (not integrated with chatroom functionality) -JM

--- ChatLoco/ChatLoco/Scripts/Chatroom/index.js
+++ ChatLoco/ChatLoco/Scripts/Chatroom/index.js
@@ -1,7 +1,6 @@
 ﻿var _findChatroomForm = $("#find-chatroom-form");
 var _chatroom = null;
 
-getGeolocation();
 _findChatroomForm.on("submit", FindChatroom)
 
 function FindChatroom(e) {
@@ -44,7 +43,7 @@ function FindChatroom(e) {
 
 }
 
-function getGeolocation() {
+function initMap() {
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -53,8 +52,44 @@ function getGeolocation() {
     }
 
     function showPosition(position) {
-        $("#lat").val(position.coords.latitude);
-        $("#lon").val(position.coords.longitude);
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude
+        $("#lat").val(lat);
+        $("#lon").val(lon);
+        
+        getNearbyPlaces(lat, lon);
+    }
+
+    function getNearbyPlaces(lat, lon) {
+        var loc = {lat: lat, lng: lon};
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: loc,
+            zoom: 19
+        });
+
+        var request = {
+            location: loc,
+            radius: '100',
+        };
+
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+
+        function callback(results, status) {
+            console.log(results);
+
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (i = 0; i < results.length; i++) {
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(results[i].geometry.location.lat(), results[i].geometry.location.lng()),
+                        map: map,
+                        animation: google.maps.Animation.DROP,
+                        title: 'Chatroom: ' + results[i].name
+                    });
+                }
+            }
+        }
     }
 
 }
