dd467af960650bdb90ac3f90ae192b26f5c08d25
cnpatton <botanicality@gmail.com>
2017-03-20 17:32:12 -0500

    Added the Settings table. Added migration and seed data

--- ChatLoco/ChatLoco/DAL/ChatlocoContext.cs
+++ ChatLoco/ChatLoco/DAL/ChatlocoContext.cs
@@ -1,5 +1,6 @@
 ﻿using ChatLoco.Entities.MessageDTO;
 using ChatLoco.Entities.UserDTO;
+using ChatLoco.Entities.SettingDTO;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -19,6 +20,8 @@ namespace ChatLoco.DAL
 
         public DbSet<UserDTO> Users { get; set; }
         public DbSet<MessageDTO> Messages { get; set; }
+        public DbSet<SettingDTO> Settings { get; set; }
+
 
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
--- ChatLoco/ChatLoco/Migrations/Configuration.cs
+++ ChatLoco/ChatLoco/Migrations/Configuration.cs
@@ -6,6 +6,7 @@ namespace ChatLoco.Migrations
     using System.Linq;
     using ChatLoco.Entities.MessageDTO;
     using ChatLoco.Entities.UserDTO;
+    using ChatLoco.Entities.SettingDTO;
     using ChatLoco.Services.Message_Service;
     using System.Collections.Generic;
     using System.Web;
@@ -40,6 +41,17 @@ namespace ChatLoco.Migrations
             };
 
             messages.ForEach(m => context.Messages.Add(m));
+            context.SaveChanges();
+
+            var settings = new List<SettingDTO>
+            {
+                new SettingDTO { Id = 0,  UserId = 0, DefaultHandle = "testUser0" },
+                new SettingDTO { Id = 1,  UserId = 1, DefaultHandle = "testUser1" },
+                new SettingDTO { Id = 2,  UserId = 2, DefaultHandle = "testUser2" },
+                new SettingDTO { Id = 3,  UserId = 3, DefaultHandle = "testUser3" },
+            };
+
+            settings.ForEach(s => context.Settings.Add(s));
             context.SaveChanges();
 
         }
